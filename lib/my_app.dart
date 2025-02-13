0d55ae7a7f43be7a0a17c19e125fc3d6d1e90a57
mostafa <mostafarasekh5@gmail.com>
2025-02-13 16:53:36 +0200

    solve gradle

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
                 darkTheme: AppTheme.dark,
                 themeMode: ThemeCubit.themeMode,
                 navigatorKey: RouteManager.navigatorKey,
-                initialRoute: PageName.winchAndWorkerScreen,
+                initialRoute: PageName.loginScreen,
                 onGenerateRoute: RouteManager.onGenerateRoute,
               );
             },
