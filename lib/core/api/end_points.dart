6052e3d77c9ee942b7280f17a383e27c607c7a04
mohamed <mohammed12atia@gmail.com>
2025-03-09 03:31:55 +0200

    refactor2

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -4,8 +4,8 @@ abstract class EndPoints {
   static const String signup = "/auth/signup";
   static const String login = "/auth/login";
   static const String profile = "/user/myprofile";
-   static const String update = "/user/updateprofile";
-   static const String delete = "/user/deleteprofile";
+  static const String update = "/user/updateprofile";
+  static const String delete = "/user/deleteprofile";
   static const String forgetPassword = "/auth/forget-password";
   static const String verifyOtp = "/auth/verify";
   static const String changePassword = "/auth/change-password";
@@ -13,5 +13,4 @@ abstract class EndPoints {
   static const String updateNote = "/user/updatereminder/1";
   static const String getNote = "/user/getreminder";
   static const String deleteNote = "/user/deletereminder/2";
-
 }
