772397e041ba239581c280da080e48ff2c043fc0
MennaAhmed24 <mennaahmed2416@gmail.com>
2025-04-16 12:06:28 +0200

    m7dsh ys7b da

--- lib/features/profile/screens/settings_screen.dart
+++ lib/features/profile/screens/settings_screen.dart
@@ -1,16 +1,11 @@
-import 'package:car_mate/config/themes/color_manager.dart';
+
 import 'package:car_mate/config/themes/text_manager.dart';
 import 'package:car_mate/config/themes/text_style.dart';
 import 'package:car_mate/core/utils/extensions/theme_extension.dart';
-import 'package:car_mate/features/alerts/presentation/pages/alerts_screen.dart';
 import 'package:car_mate/features/notifications/presentation/pages/notification_screen.dart';
 import 'package:car_mate/features/repair/presentation/views/own_posts_screen.dart';
-import 'package:car_mate/features/repair/presentation/widgets/custom_listview_own_posts.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../profile_cubit/profile_cubit.dart';
 import '../widgets/delete_button.dart';
 import '../widgets/logout_button.dart';
 import '../widgets/profile_header.dart';
